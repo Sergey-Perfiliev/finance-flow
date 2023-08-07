0e9ce4193d01634489d10fd641fd61c1fd12122e
Sergey-Perfiliev <sergeyperfiliev001@gmail.com>
2023-08-07 23:30:58 +0300

    add: basic styles for typegraphy, vars, global

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import './App.css'
-
-function App() {
-	return <h1>Finance Flow</h1>
+const App = () => {
+	return (
+		<div>
+			<h1 className='heading-primary'>Finance Flow</h1>
+			<h2 className='heading-secondary'>Heading secondary</h2>
+			<h3 className='heading-tertiary'>Heading tertiary</h3>
+		</div>
+	)
 }
 
 export default App
