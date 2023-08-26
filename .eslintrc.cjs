511a0e1dc246ff9b28db0732bcde19ec476ec2af
Sergey-Perfiliev <sergeyperfiliev001@gmail.com>
2023-08-26 23:24:21 +0300

    refactor: .eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,29 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react-hooks/recommended',
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parser: '@typescript-eslint/parser',
+	plugins: ['react-refresh'],
+	rules: {
+		'react-refresh/only-export-components': [
+			'warn',
+			{ allowConstantExport: true },
+		],
+		'@typescript-eslint/no-explicit-any': ['off'],
+		'no-restricted-imports': 'off',
+		'@typescript-eslint/no-restricted-imports': [
       'warn',
-      { allowConstantExport: true },
-    ],
-    "@typescript-eslint/no-explicit-any": ["off"]
-  },
+			{
+				name: 'react-redux',
+				importNames: ['useSelector', 'useDispatch'],
+				message:
+					'Use typed hooks `useAppDispatch` and `useAppSelector` instead.',
+			},
+		],
+	},
 }
